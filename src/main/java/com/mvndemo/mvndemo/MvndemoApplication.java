035217e643cd079a202e2835b8a80c7e3317c91d
houxinwei <skyiw@foxmail.com>
2023-03-13 17:41:55 +0800

    'Test1'

---
+++ src/main/java/com/mvndemo/mvndemo/MvndemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvndemo.mvndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvndemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvndemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvndemo/mvndemo/MvndemoApplicationTests.java
@@ -0,0 +1,29 @@
+package com.mvndemo.mvndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes =  MvndemoApplication.class)
+class MvndemoApplicationTests implements ApplicationContextAware {
+
+
+	private ApplicationContext applicationContext;
+	@Test
+	void contextLoads() {
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Test
+	public void testApplicationContext(){
+		System.out.println(applicationContext);
+	}
+}
